ddb34d80ed8d53392a5aa6ce791e515e934059ec
Vnganm <161715774+Vnganm@users.noreply.github.com>
2025-06-24 15:28:58 +0700

    add Lab4

---
+++ LAB4/lab4/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Quiz from './Quiz';
+
+
+function App() {
+  return (
+    <div>
+      <Quiz/>
+    </div>
+  );
+}
+
+export default App;
